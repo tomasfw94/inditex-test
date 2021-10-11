4fa6efba2a74f606500fe9a0f451feed2706034b
tomasfw94 <tomasfw94@gmail.com>
2021-10-11 19:24:32 +0200

    ProductPriceDto refactor

--- src/main/java/com/inditex/inditextest/dto/ProductPriceDto.java
+++ src/main/java/com/inditex/inditextest/dto/ProductPriceDto.java
@@ -1,16 +1,22 @@
 package com.inditex.inditextest.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductPriceDto {
+
     private int productId;
     private int brandId;
-    private BigDecimal feeToApply;
+    private int priceList;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private BigDecimal pvp;
+
 }
