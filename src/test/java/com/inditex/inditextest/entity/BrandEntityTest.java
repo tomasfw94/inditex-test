4e1d12e591a29d241106533b063d2681754d8fcc
tomasfw94 <tomasfw94@gmail.com>
2021-10-13 00:29:52 +0200

    Entities test - Added correct JUnit import

--- src/test/java/com/inditex/inditextest/entity/BrandEntityTest.java
+++ src/test/java/com/inditex/inditextest/entity/BrandEntityTest.java
@@ -1,12 +1,15 @@
 package com.inditex.inditextest.entity;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
 public class BrandEntityTest {
 
